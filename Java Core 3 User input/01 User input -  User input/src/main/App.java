6b3c25ed2bb0b5bc3287170240c4eef1aaa6649c
Adamo <adamo.audebert@gmail.com>
2023-06-02 18:57:15 +0200

    user input

---
+++ Java Core 3 User input/01 User input -  User input/src/main/App.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Whats your age ?");
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		System.out.println("Your age is "+age);
+		sc.close();
+
+	}
+
+}
